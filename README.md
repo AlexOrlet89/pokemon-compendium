5e8d9e6b4f5acef7faa82a0b1980fb7f0d479018
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-16 15:15:31 -0700

    useState creates a function with a state variable of pokemon, and a default of an empty array. useEffect runs a call back function that grabs data from our API and sends it to the setter function, which updates our state variables. the depency is an empty array and means, ultimately, we only update our state once upon load. that state variable is a list of pokemon we get thru our fetch pokemon function, that list is then mapped and becomes what our Main function returns

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Compendium Planning
 
 - Get my list of pokemon showing up on the page
-  1. write your fetch function -- validate with console.log
-  1. addUseEffect and set state with the response
+  1. write your fetch function -- validate with console.log //check
+  1. add UseEffect and set state with the response
   1. map through your state and display the name
   1. build out your PokeCard component -- use static values
   1. map through your items and display PokeCard
--- src/views/Main.js
+++ src/views/Main.js
@@ -3,9 +3,20 @@ import { useEffect, useState } from 'react';
 import { fetchPokemon } from '../services/pokemon';
 
 export default function Main() {
+  const [pokemon, setPokemon] = useState([]);
   useEffect(() => {
-    fetchPokemon();
+    const fetchData = async () => {
+      const data = await fetchPokemon();
+      setPokemon(data);
+    };
+    fetchData();
   }, []);
 
-  return <div>Main</div>;
+  return (
+    <div>
+      {pokemon.map((pokeman) => (
+        <p key={pokeman.id}>{pokeman.pokemon}</p>
+      ))}
+    </div>
+  );
 }
