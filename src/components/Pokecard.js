d743950dec33af0c6b1c3af177eb977c404144a7
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-18 12:01:23 -0700

    cssing in progress

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,12 +1,28 @@
 import './Pokecard.css';
 
-export default function PokeCard({ pokemon, type_1, type_2 }) {
+export default function PokeCard({
+  pokemon,
+  type_1,
+  type_2,
+  color_1,
+  color_2,
+  color_f,
+  url_image,
+}) {
   return (
-    <div className="pokecard">
-      <p>name: {pokemon}</p>
-      <p>
+    <div
+      className="pokecard"
+      style={{
+        backgroundColor: `${color_1}`,
+        border: `${color_2} 5px solid`,
+        borderRadius: `25px`,
+      }}
+    >
+      <p style={{ alignSelf: `flex-start` }}>
+        {pokemon} <br></br>
         types: {type_1}, {type_2}
       </p>
+      <img src={`${url_image}`} alt={`${url_image}`} />
     </div>
   );
 }
